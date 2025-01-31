7c7f160ec87da1d914e4fb2c6d62214bc3171bde
michael <michael.norton1213@gmail.com>
2025-01-31 09:44:42 -0500

    https

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     useEffect(() => {
         async function logErrorToServer(message) {
             try {
-                await axios.post("http://https://playlist-api-mu.vercel.app/error", { msg: message });
+                await axios.post("https://playlist-api-mu.vercel.app/error", { msg: message });
                 console.log("Error logged successfully.");
             } catch (error) {
                 console.error("Failed to log error:", error);
